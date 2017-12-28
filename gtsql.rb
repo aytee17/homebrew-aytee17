337991698274fc95827bd0ac70ddbf5db62165be
Andrew Tang <aytee17.github@outlook.com>
2017-12-28 17:04:00 +1000

    Create gtsql.rb

---
+++ gtsql.rb
@@ -0,0 +1,13 @@
+require "formula"
+
+class Gtsql < Formula
+  homepage "https://github.com/aytee17/gtfs-to-sqlite"
+  url "https://github.com/aytee17/gtfs-to-sqlite/releases/download/v0.0.1-beta/gtfs-to-sqlite-0.0.1.zip"
+  sha256 "4fd6e57ce509c09cddd534d751c281a461bbc10871371205d07a65e3ea921d83"
+
+  def install
+  	inreplace "brew/gtsql", "##PREFIX##", "#{prefix}"
+  	prefix.install "gtfs-to-sqlite-0.0.1-all.jar"
+  	bin.install "brew/gtsql"
+  end
+end
