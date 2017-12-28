35f93e0d5a0bd954bae52b0ca7ebcd05e4125346
Andrew Tang <aytee17.github@outlook.com>
2017-12-28 17:40:21 +1000

    Update gtsql.rb

--- gtsql.rb
+++ gtsql.rb
@@ -3,7 +3,7 @@ require "formula"
 class Gtsql < Formula
   homepage "https://github.com/aytee17/gtfs-to-sqlite"
   url "https://github.com/aytee17/gtfs-to-sqlite/releases/download/v0.0.1-beta/gtfs-to-sqlite-0.0.1.zip"
-  sha256 "5c3e0247a222ab24a44e43e6d9d641dcc24c6d80"
+  sha256 "db91ea25e49bda7507cfeeae4cc1c4b86187b3db53db9956f8a30a9a845f1fe1"
 
   def install
   	inreplace "brew/gtsql", "##PREFIX##", "#{prefix}"
